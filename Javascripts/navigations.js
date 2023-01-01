6fbb6c042c192dcc7f21fd5349beaf6d77ece79d
root-moody <moodyamin531@gmail.com>
2023-01-01 10:15:51 +0300

    login template

--- Javascripts/navigations.js
+++ Javascripts/navigations.js
@@ -14,4 +14,7 @@ $(document).ready(function () {
     $("#news").click(function(){
         $("#bodydiv").load("../pages/news.html")
     });
+    $("#login").click(function(){
+        $("#bodydiv").load("../pages/login.html")
+    });
 });
