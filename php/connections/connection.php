9ce66af3f2e61f8b7f5e3ac52fda6c651b83d7e8
root-moody <moodyamin531@gmail.com>
2023-01-05 07:02:23 +0300

    connections

---
+++ php/connections/connection.php
@@ -0,0 +1,10 @@
+<?php
+
+ $conn = new mysqli('localhost', 'Administrator', 'zero123tech','schoolproject');
+
+    if(! $conn )  
+    {  
+    die('Could not connect: ' . mysqli_error());  
+    }
+
+?>
