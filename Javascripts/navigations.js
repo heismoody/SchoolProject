f3d271bddc5eec6948fed009d6aaa1b594cef403
Moody Amin <moodyamin531@gmail.com>
2023-01-10 07:35:46 +0300

    minor

--- Javascripts/navigations.js
+++ Javascripts/navigations.js
@@ -1,27 +1,26 @@
-$(document).ready(function () {
-    $("#home").click(function(){
+$(document).ready(function() {
+    $("#home").click(function() {
         location.reload();
     });
-    $("#about").click(function () {
-        alert("im testing out");
+    $("#about").click(function() {
         $("#bodydiv").load("./pages/aboutschool.html")
     });
-    $("#gallery").click(function(){
+    $("#gallery").click(function() {
         $("#bodydiv").load("./pages/gallery.html")
     });
-    $("#results").click(function(){
+    $("#results").click(function() {
         $("#bodydiv").load("./pages/results.html")
     });
-    $("#news").click(function(){
+    $("#news").click(function() {
         $("#bodydiv").load("./pages/news.html")
     });
-    $("#login").click(function(){
+    $("#login").click(function() {
         $("#bodydiv").load("./pages/login.html")
     });
-    $("#teacherpanel").click(function(){
+    $("#teacherpanel").click(function() {
         $("#bodydiv").load("./pages/teacherpanel.html")
     });
-    $("#adminpanel").click(function(){
+    $("#adminpanel").click(function() {
         $("#bodydiv").load("./pages/admindashboard.html")
     });
 });
