63a155c53ec11b19178b93ca4f17a187ec7b5ed2
Moody Amin <moodyamin531@gmail.com>
2023-01-22 15:19:34 +0300

    struggling

---
+++ pages/images.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        include '../php/navigation.php';
+    ?>
+    <div class="d-flex justify-content-center w-100" style="background-color: rgb(233, 233, 233);">
+        <section class="navwid">
+            <div class="row container">
+                <div class="col-sm imageframe">
+                    <img src="../uploads/gallery/moody/joh.jpg" alt="" srcset="" class="respimg">
+                </div>
+                <div class="col-sm imageframe">
+                    <img src="../uploads/gallery/moody/tzlife.jpg" alt="" srcset="" class="respimg">
+                </div>
+                <div class="col-sm imageframe">
+                    <img src="../uploads/gallery/moody/kim.jpg" alt="" srcset="" class="respimg">
+                </div>
+            </div>
+        </section>
+    </div>
+</body>
+</html>
